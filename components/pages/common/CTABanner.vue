d42b71067935c08b0a8608432f3cd427635362af
Syed Saqib Ali <Saqibsyed1014@gmail.com>
2024-08-04 01:02:43 +0500

    CTA Banner text updated

--- components/pages/common/CTABanner.vue
+++ components/pages/common/CTABanner.vue
@@ -1,23 +1,18 @@
-<script setup lang="ts">
-
-import BaseButton from "~/components/core/BaseButton.vue";
-</script>
-
 <template>
   <div class="cta-banner">
     <div class="container">
-      <div class="banner relative bg-brand-800 rounded-[16px] overflow-hidden py-10 md:py-16 px-6 md:px-12">
+      <div class="banner relative bg-brand-800 rounded-2xl overflow-hidden py-10 md:py-16 px-6 md:px-12">
         <div class="flex max-md:flex-col justify-between relative gap-8 z-20">
           <div class="heading-and-subheading flex flex-col gap-4 md:gap-2">
             <h4 class="text-3xl text-white font-semibold">
-              We believe in supporting the community
+              Hire Top Talent Effortlessly
             </h4>
             <p class="text-brand-200 text-lg pb-3">
-              We donate a percentage of our revenue to non-profit organizations
+              Discover how our platform can streamline your hiring process and connect you with the top talent.
             </p>
           </div>
           <div class="shrink-0">
-            <BaseButton label="Our Pledge" color="primary" :full-sized-on-small="true" />
+            <BaseButton label="Request a Demo" navigate-to="/demo-request" color="primary" :full-sized-on-small="true" />
           </div>
         </div>
 
@@ -29,7 +24,3 @@ import BaseButton from "~/components/core/BaseButton.vue";
     </div>
   </div>
 </template>
-
-<style scoped lang="postcss">
-
-</style>
